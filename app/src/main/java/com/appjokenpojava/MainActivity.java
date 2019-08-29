21b93b12e1768869bc64c1347103bed5cb6259e8
Jefferson <jeffersonbds_barros@hotmail.com>
2019-08-29 16:20:05 -0300

    Tela inicial reformulada

--- app/src/main/java/com/appjokenpojava/MainActivity.java
+++ app/src/main/java/com/appjokenpojava/MainActivity.java
@@ -2,8 +2,21 @@ package com.appjokenpojava;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+    private Button botaoPapel;
+    private Button botaoPedra;
+    private Button botaoTesoura;
+    private Button botaoUltimasJogadas;
+
+    private EditText textoResultado;
+
+    private ImageView imagemComputador;
+    private ImageView imagemJogador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
