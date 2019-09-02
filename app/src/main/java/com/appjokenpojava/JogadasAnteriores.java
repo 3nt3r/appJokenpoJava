f1824949c9ab25cad18a1e98d207fdd97a9c275c
Jefferson <jeffersonbds_barros@hotmail.com>
2019-09-02 11:34:56 -0300

    Implementação do listview

--- app/src/main/java/com/appjokenpojava/JogadasAnteriores.java
+++ app/src/main/java/com/appjokenpojava/JogadasAnteriores.java
@@ -1,13 +1,29 @@
 package com.appjokenpojava;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Collections;
 
 public class JogadasAnteriores extends AppCompatActivity {
+
+    private DatabaseReference databaseReferencia = FirebaseDatabase.getInstance().getReference();
+    private DatabaseReference jogadasReferencia = databaseReferencia.child("ultimasJogadas");
+
+    ListView listaJogadas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +34,54 @@ public class JogadasAnteriores extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setTitle("Últimas Jogadas");
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.corBarraTitulo)));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        listaJogadas = findViewById(R.id.listaJogadasId);
+
+
+        String[] dados = new String[] { "Cupcake", "Donut", "Eclair", "Froyo", "Gingerbread",
+                "Honeycomb", "Ice Cream Sandwich", "Jelly Bean",
+                "KitKat", "Lollipop", "Marshmallow", "Nougat" };
+
+        jogadasReferencia.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                dataSnapshot.getValue().toString();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dados);
+
+        listaJogadas.setAdapter(adapter);
+
+
+
+
+
+
+
+
+
 
 
 
